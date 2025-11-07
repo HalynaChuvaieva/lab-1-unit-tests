eb6ab144c8cfef89d9a7c6db01bbe7acfebb927b
Halyna <galacuvaeva4@gmail.com>
2025-11-08 00:19:34 +0200

    add coments for TestBalancedBST

--- TestBalancedBST.h
+++ TestBalancedBST.h
@@ -1,7 +1,17 @@
 #pragma once
 #include <iostream>
 #include "BalancedBST.h"
-
+/**
+ * @brief Concrete class extending BalancedBST for testing insertion logic.
+ * 
+ * @tparam T The data type stored in the tree.
+ * @example
+ * TestBalancedBST<int> tree;
+ * tree.insert(10);
+ * tree.insert(5);
+ * tree.insert(15);
+ * cout << "Tree height: " << tree.getHeight() << endl;
+ */
 template <class T>
 class TestBalancedBST : public BalancedBST<T>
 {
@@ -9,12 +19,23 @@ class TestBalancedBST : public BalancedBST<T>
     using HeightNode = typename BalancedBST<T>::HeightNode;
 
 public:
+    /**
+     * @brief Inserts a new value into the balanced tree.
+     * @param value The value to insert.
+     */
+
     void insert(T value) override
     {
         this->root = insertRec(static_cast<HeightNode *>(this->root), value);
     }
 
 private:
+    /**
+     * @brief Recursive helper for insertion maintaining node heights.
+     * @param node Current node pointer.
+     * @param value The value to insert.
+     * @return Updated node pointer.
+     */
     HeightNode *insertRec(HeightNode *node, T value)
     {
         if (!node)
